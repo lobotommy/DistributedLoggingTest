c5c28540c96482244d5698ad38974f60df7607fe
lobotommy <wendelborg@gmail.com>
2015-03-13 19:38:12 +0100

    V1, konsoll og json

---
+++ Log4Json/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using log4net;
+
+namespace Log4Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetLogger("Logger");
+            for (var cnt = 0; cnt < 100; cnt++)
+            {
+                var sw = Stopwatch.StartNew();
+                logger.Debug("Test " + cnt);
+                sw.Stop();
+                Console.WriteLine();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
